659e08730a5c14cbfa1fad4c5b139c4701fddf77
ereperezq-pixel <ereperezq@gmail.com>
2026-09-09 01:37:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "english-fun-start-v20";
+const CACHE_NAME = "english-fun-start-v21";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./conversation.js", "./iphone.css", "./iphone.js"];
 
 self.addEventListener("install", event => {
